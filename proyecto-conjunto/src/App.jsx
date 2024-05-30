d37c752fdcd93b677ec0496ab6d1f61803038813
Jesus Jorge Varela Perez <jvarelape@d4dom.net>
2024-05-30 20:18:34 +0200

    3005 Jorge

--- proyecto-conjunto/src/App.jsx
+++ proyecto-conjunto/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import  {Buttonjorge} from './components/ComponenteJorge'
+import { Title } from './components/ComponenteJorgePersonalizado'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +13,11 @@ function App() {
    
       <div>
       <Buttonjorge />
+
+    <Title>
+       Hello World!
+     </Title>
+
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
