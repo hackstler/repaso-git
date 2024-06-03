b062d5bdd81e4958f832c908b0701a5e4e7d3754
sergio <sergio@hackstler.com>
2024-06-03 20:19:36 +0200

    fix deploy without image

--- proyecto-conjunto/src/App.jsx
+++ proyecto-conjunto/src/App.jsx
@@ -1,32 +1,18 @@
-
-
-
-import './App.css'
-import  {Buttonjorge} from './components/ComponenteJorge'
-import { Title } from './components/ComponenteJorgePersonalizado'
-import {ImageComponent} from './components/ComponentsDani'
-import { ButtonHelen } from './components/ComponenteElena'
-import CustomSwitch from './components/component-barb'
+import "./App.css";
+import { Buttonjorge } from "./components/ComponenteJorge";
+import { Title } from "./components/ComponenteJorgePersonalizado";
+import { ButtonHelen } from "./components/ComponenteElena";
+import CustomSwitch from "./components/component-barb";
 
 function App() {
   return (
     <>
-   <ImageComponent
-        src="https://res.cloudinary.com/dq186ej4c/image/upload/v1689761508/pngwing.com_r0hr9b.png"
-        alt="Description of image"
-        width="300px"
-        height="200px"
-        className="my-image-class"
-      />
       <Buttonjorge />
       <Title> HEllo World </Title>
       <ButtonHelen />
       <CustomSwitch />
-     
-      
     </>
-  )
+  );
 }
 
-
-export default App
+export default App;
