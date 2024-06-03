5e9f1ce52543dcfe72ebbd8b127c4efa2a7532bf
bbucarelo <b.bucarelo@gmail.com>
2024-06-03 19:23:53 +0200

    my component

--- proyecto-conjunto/src/App.jsx
+++ proyecto-conjunto/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { ChakraProvider } from '@chakra-ui/react'
 import './App.css'
 
 function App() {
@@ -8,6 +9,8 @@ function App() {
 
   return (
     <>
+    <ChakraProvider>
+    
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -28,6 +31,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+    </ChakraProvider>
     </>
   )
 }
